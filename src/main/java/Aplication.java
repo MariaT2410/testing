7b6a4a151e10f3e14639f66a0579c4f8a6ef7f9c
29_06 <29_06_titova@mail.ru>
2023-04-29 13:14:27 +0300

    Начало

---
+++ src/main/java/Aplication.java
@@ -0,0 +1,34 @@
+import animals.Kotik;
+
+public class Aplication {
+    public static void main(String[] arg){
+        System.out.println("Добро пожаловать в котоДОМ");
+        //создаём котика 1
+        Kotik k = new Kotik("Зефирка", "мур-мяу", 0, 4900);
+
+        //создаём котика2
+        Kotik kotik2 = new Kotik();
+        kotik2.setName("Печенька");
+        kotik2.setVoice("ммррррр");
+        kotik2.setSatiety(6);
+        kotik2.setWeight(3600);
+
+        //Жизненный путь котика 1 за день
+        k.liveAnotherDay();
+
+        // Имя + Вес котика
+        System.out.println(k.getName() + " " + k.getWeight());
+
+        //Сравнение голосов котиков
+        compareVoice(k, kotik2);
+
+        //количество созданных котиков
+        Kotik.getCount();
+    }
+    private static void compareVoice(Kotik m1, Kotik m2){
+        if (m1.getVoice().equals(m2.getVoice())){
+            System.out.println("Котики мяукают одинаково");
+        }
+        else System.out.println("Котики мяукают по-разному");
+    }
+}
---
+++ src/main/java/animals/Kotik.java
@@ -0,0 +1,171 @@
+package animals;
+
+public class Kotik {
+    private String name;
+    private String voice;
+    private int satiety; // сытость [0;10]
+    private int weight;
+    private String food;
+
+    private static int count = 0;
+    private static int METHODS = 5;
+
+    //Конструкторы
+    public Kotik(String name, String voice, int satiety, int weight) {
+        this.name = name;
+        this.voice = voice;
+        this.satiety = satiety;
+        this.weight = weight;
+        count++;
+    }
+    public Kotik() {
+        count++;
+    }
+
+    // Getter and Setter для полей name, voice, satiety, weight
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getVoice() {
+        return voice;
+    }
+    public void setVoice(String voice) {
+        this.voice = voice;
+    }
+    public int getSatiety() {
+        return satiety;
+    }
+    public void setSatiety(int satiety) {
+        this.satiety = satiety;
+    }
+    public int getWeight() {
+        return weight;
+    }
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+    public String getFood() {
+        return food;
+    }
+    public void setFood(String food) {
+        this.food = food;
+    }
+
+
+    // Определение методов play (играть), sleep (спать), wash (умываться), walk (гулять), hunt (охотиться)
+    public boolean play(){
+        if( satiety>0){
+            System.out.println("Котик играет");
+            // случайно определяем сколько единиц сытости ушло во время play()
+            int randomSatiety = (int) (Math.random() * 10);
+            this.satiety -=randomSatiety;
+            return true;
+        } else {
+            eat((int) (Math.random() * 10));
+            return false;
+        }
+    }
+    public boolean sleep(){
+        if( satiety>0){
+            System.out.println("Котик спит");
+            // случайно определяем сколько единиц сытости ушло во время sleep()
+            int randomSatiety = (int) (Math.random() * 10);
+            this.satiety -=randomSatiety;
+            return true;
+        } else {
+            eat((int) (Math.random() * 10));
+            return false;
+        }
+
+    }
+    public boolean wash(){
+        if( satiety>0){
+            System.out.println("Котик умывается");
+            // случайно определяем сколько единиц сытости ушло во время wash()
+            int randomSatiety = (int) (Math.random() * 10);
+            this.satiety -=randomSatiety;
+            return true;
+        } else {
+            eat((int) (Math.random() * 10));
+            return false;
+        }
+    }
+    public boolean walk(){
+        if( satiety>0){
+            System.out.println("Котик гуляет");
+            // случайно определяем сколько единиц сытости ушло во время walk()
+            int randomSatiety = (int) (Math.random() * 10);
+            this.satiety -=randomSatiety;
+            return true;
+        } else {
+            eat((int) (Math.random() * 10));
+            return false;
+        }
+    }
+
+    //Для выведения количества итераций собзания объекта animals.Kotik
+    public static int getCount(){
+        System.out.println(count);
+        return count;
+    }
+
+    public boolean hunt(){
+        if( satiety>0){
+            System.out.println("Котик охотится");
+            // случайно определяем сколько единиц сытости ушло во время hunt()
+            int randomSatiety = (int) (Math.random() * 10);
+            this.satiety -=randomSatiety;
+            return true;
+        } else {
+            eat((int) (Math.random() * 10), "Мышка");
+            return false;
+        }
+    }
+
+    // Перегрузка методов eat
+    public void eat(int satiety){
+        System.out.println("Кормим котика");
+        this.satiety += satiety;
+    }
+    public void eat(int satiety, String food){
+        System.out.println("Кормим котика. Еда:" + food);
+        this.satiety += satiety;
+    }
+    public void eat(){
+        System.out.print("Вызываем метод кормления food");
+        eat(satiety, food);
+    }
+
+    public void liveAnotherDay(){
+
+        System.out.println("Очень важные дела котика по имени "+this.name+" за весь день:");
+        for (int i=0; i<24; i++){
+            int a = (int) (Math.random() * METHODS) + 1;
+            switch (a){
+                case 1:
+                    System.out.print(i+" - ");
+                    play();
+                    break;
+                case 2:
+                    System.out.print(i+" - " );
+                    sleep();
+                    break;
+                case 3:
+                    System.out.print(i+" - " );
+                    wash();
+                    break;
+                case 4:
+                    System.out.print(i+" - " );
+                    walk();
+                    break;
+                case 5:
+                    System.out.print(i+" - " );
+                    hunt();
+                    break;
+            }
+        }
+    }
+}
